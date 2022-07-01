4fb5770fccbd3cac3b1779421535573789842d39
Edu Marin <monocle.md@gmail.com>
2022-07-01 11:55:47 -0300

    funcionamiento envio de mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
 
 
 router.get('/', function (req, res, next) {
@@ -8,4 +10,39 @@ router.get('/', function (req, res, next) {
     }); // view/contacto.hbs
 })
 
+router.post('/', async function(req,res,next){
+    /* console.log(req.body) */
+
+//capturando los datos//
+var nombre = req.body.nombre; //1
+var email = req.body.email; //2
+var tel = req.body.tel; //3
+var comentarios = req.body.comentarios; //4
+
+var obj  = {
+    to:"billedu@gmail.com",
+    subject:"CONTACTO DESDE PAG WEB",
+    html: nombre + " se contactó a través de la web y quiere saber más info a este correo: " + email + "<br> Su tel es: " + tel + "<br>. Su cometario es: " + comentarios
+}
+
+var transport = nodemailer.createTransport ({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+}
+
+}) //finaliza el trasnport
+
+var info = transport.sendMail(obj);
+
+res.render('contacto', {
+message: "Mensaje enviado correctamente",
+isContacto: true
+})
+
+}) // finaliza router.post
+
 module.exports = router;
+
