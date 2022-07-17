339b2a0aea114eac4028b3ce1b7cdb229ea00478
Edu Marin <monocle.md@gmail.com>
2022-07-17 13:33:18 -0300

    novedades desde el front dinamicas

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
 
 
-router.get('/', function (req, res, next) {
+
+router.get('/', async function (req, res, next) {
+    
+var novedades = await novedadesModel.getNovedades();
+
+
     res.render('novedades', {
-        isNovedades: true
-    }); // view/novedades.hbs
+        isNovedades: true,
+        novedades //tengo todos los registros para poder eimprimirlos en el hbs//
+    }); 
 })
 
 module.exports = router;
