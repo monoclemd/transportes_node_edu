21f7f9fbc22f1013645e2f13348df4417348f3d8
Edu Marin <monocle.md@gmail.com>
2022-07-18 11:50:16 -0300

    buscador novedades Front

--- routes/novedades.js
+++ routes/novedades.js
@@ -6,13 +6,25 @@ var novedadesModel = require('../models/novedadesModel');
 
 router.get('/', async function (req, res, next) {
     
-var novedades = await novedadesModel.getNovedades();
+//var novedades = await novedadesModel.getNovedades();
+
+var novedades;
+if(req.query.q === undefined){
+    novedades = await novedadesModel.getNovedades();
+} else {
+    novedades = await novedadesModel.buscarNovedades(req.query.q);
+}
 
 
     res.render('novedades', {
         isNovedades: true,
-        novedades //tengo todos los registros para poder eimprimirlos en el hbs//
+        novedades, //tengo todos los registros para poder eimprimirlos en el hbs//
+        is_search: req.query.q !== undefined,
+        q:req.query.q
     }); 
 })
+
+
+
 
 module.exports = router;
